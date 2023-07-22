595d495b46909417c047a22acbe573a33aaa035a
root <root@ESG11C-DKEL>
2023-07-22 06:40:26 -0700

    Made change to deselect marker

--- app/components/objectiveMarker.tsx
+++ app/components/objectiveMarker.tsx
@@ -24,23 +24,35 @@ const objMarker: FC<objMarkerProps> = (props) => {
   
     let [circleColour, setCircleColour] = useState(`rgb(${props.player1colour["red"]},${props.player1colour["green"]},${props.player1colour["blue"]}`); 
       
-    const onClickObj = (player:string) => {
-      // fetch(`http://${props.IP}/led/player?p=${player}`, {method: 'GET'})
-      // .then(response => response.text())
-      // .then(text => console.log(text))
-      // .catch(error => console.log('error', error));
+    const onClickObj = (player:string, id: number) => {
+
+      let playerID = id;
 
       if (player === props.player1) {
-        setCircleColour(`rgb(${props.player1colour["red"]},${props.player1colour["green"]},${props.player1colour["blue"]}`);
+        if (circleColour !== `rgb(${props.player1colour["red"]},${props.player1colour["green"]},${props.player1colour["blue"]}`){
+          setCircleColour(`rgb(${props.player1colour["red"]},${props.player1colour["green"]},${props.player1colour["blue"]}`)
+        }else {
+          playerID = 0;
+          setCircleColour(`rgb(0,0,0`);
+        }
       } else {
-        setCircleColour(`rgb(${props.player2colour["red"]},${props.player2colour["green"]},${props.player2colour["blue"]}`);
-  
+        if (circleColour !== `rgb(${props.player2colour["red"]},${props.player2colour["green"]},${props.player2colour["blue"]}`){
+          setCircleColour(`rgb(${props.player2colour["red"]},${props.player2colour["green"]},${props.player2colour["blue"]}`);
+        }else {
+          playerID = 0;
+          setCircleColour(`rgb(0,0,0`);
+        }
       }
+
+      fetch(`http://${props.IP}/led/player?p=${playerID}`, {method: 'GET'})
+      .then(response => response.text())
+      .then(text => console.log(text))
+      .catch(error => console.log('error', error));
     }
 
     return (
       <div className ="flex justify-center">
-          <button className="px-6 py-3.5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 my-5" onClick={() => onClickObj(props.player1)}>Set Obj for: {props.player1}</button>
+          <button className="px-6 py-3.5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 my-5" onClick={() => onClickObj(props.player1, props.id)}>Set Obj for: {props.player1}</button>
         <div className="flex justify-center items-center rounded-full mx-20"
               style={{
                   width: '80px',
@@ -48,7 +60,7 @@ const objMarker: FC<objMarkerProps> = (props) => {
                   borderRadius: '50%',
                   backgroundColor: circleColour,
               }}><span>{props.id}</span></div>
-          <button className ="px-6 py-3.5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 my-5" onClick={() => onClickObj(props.player2)}>Set Obj for: {props.player2}</button>
+          <button className ="px-6 py-3.5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 my-5" onClick={() => onClickObj(props.player2, props.id)}>Set Obj for: {props.player2}</button>
       </div>
     );
   }
