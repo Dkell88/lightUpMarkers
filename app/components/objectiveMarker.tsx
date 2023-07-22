71b699b5a298a796beca3fdfce0950ab8e64e559
root <root@ESG11C-DKEL>
2023-07-22 06:42:52 -0700

    Fix default color

--- app/components/objectiveMarker.tsx
+++ app/components/objectiveMarker.tsx
@@ -22,7 +22,7 @@ interface objMarkerProps {
 
 const objMarker: FC<objMarkerProps> = (props) => {
   
-    let [circleColour, setCircleColour] = useState(`rgb(${props.player1colour["red"]},${props.player1colour["green"]},${props.player1colour["blue"]}`); 
+    let [circleColour, setCircleColour] = useState(`rgb(0,0,0`); 
       
     const onClickObj = (player:string, id: number) => {
 
@@ -53,7 +53,7 @@ const objMarker: FC<objMarkerProps> = (props) => {
     return (
       <div className ="flex justify-center">
           <button className="px-6 py-3.5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 my-5" onClick={() => onClickObj(props.player1, props.id)}>Set Obj for: {props.player1}</button>
-        <div className="flex justify-center items-center rounded-full mx-20"
+        <div className="flex justify-center items-center rounded-full mx-20 text-white"
               style={{
                   width: '80px',
                   height: '80px',
